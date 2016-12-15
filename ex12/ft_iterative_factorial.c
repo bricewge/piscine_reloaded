7a31b5f862355a7dd4a286da8b15df119ec9e2cc
Brice Waegeneire <brice.wge@gmail.com>
2016-12-15 14:24:50 +0100

    ex12 add error control

--- ex12/ft_iterative_factorial.c
+++ ex12/ft_iterative_factorial.c
@@ -6,7 +6,7 @@
 /*   By: bwaegene <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/15 14:10:09 by bwaegene          #+#    #+#             */
-/*   Updated: 2016/12/15 14:18:23 by bwaegene         ###   ########.fr       */
+/*   Updated: 2016/12/15 14:24:09 by bwaegene         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@ int	ft_iterative_factorial(int nb)
 {
 	int result;
 
+	if (nb < 0)
+		return (0);
 	result = 1;
 	while (nb)
 		result *= nb--;
