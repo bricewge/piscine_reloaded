404259b2c5882cdfe970577dbcffd458796795a4
Brice Waegeneire <brice.wge@gmail.com>
2016-12-16 13:56:37 +0100

    ex20

---
+++ ex20/ft_strdup.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bwaegene <brice.wge@gmail.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/07/08 00:55:54 by bwaegene          #+#    #+#             */
+/*   Updated: 2016/12/16 13:55:27 by bwaegene         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int		ft_strlen(char *str)
+{
+	int		i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strcpy(char *dest, char *src)
+{
+	int		i;
+
+	i = 0;
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+char	*ft_strdup(char *src)
+{
+	char	*dup;
+
+	dup = (char*)malloc(sizeof(*dup) * ft_strlen(src) + 1);
+	return (ft_strcpy(dup, src));
+}
